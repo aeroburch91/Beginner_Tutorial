6aef2ea2675c064f95f2c84a319313631e834f43
aeroburch91 <ryankburch@gmail.com>
2020-02-23 08:55:21 -0600

    Initial commit

---
+++ src/Flight.java
@@ -0,0 +1,34 @@
+public class Flight {
+    private int passengers;
+    private int seats;
+
+    public Flight() {
+        seats = 150;
+        passengers = 0;
+    }
+
+    public void add1Passenger () {
+        if (passengers < seats)
+            passengers += 1;
+        else
+            handleTooMany();
+    }
+
+    public boolean hasRoom(Flight f2) {
+        int total = passengers + f2.passengers;
+        return total <= seats;
+    }
+
+    public Flight createNewWithBoth (Flight f2) {
+        Flight newFlight = new Flight();
+        newFlight.seats = seats;
+        newFlight.passengers = passengers + f2.passengers;
+        return newFlight;
+    }
+
+    private void handleTooMany () {
+        System.out.println("Too Many Passengers");
+    }
+}
+
+
---
+++ src/FlightBuilder.java
@@ -0,0 +1,12 @@
+public class FlightBuilder {
+    Flight lax1 = new Flight();
+    Flight lax2 = new Flight();
+
+    // add passengers
+
+    lax1.add1Passenger;
+
+    Flight lax3;
+    if(lax1.hasRoom(lax2))
+
+}
